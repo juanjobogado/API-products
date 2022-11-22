61fb7c1bec1d5fce0f672bb9065d733344bf0c7f
juanjobogado <juanjobogado13@gmail.com>
2022-11-22 19:46:24 -0300

    almost id ready

--- src/controllers/idProduct.controller.js
+++ src/controllers/idProduct.controller.js
@@ -1,2 +1,19 @@
 import { product } from "../models/products.js"
+import { Op } from "sequelize";
 
+export const idProduct = async (req,res) => {
+    try{
+        const { id } = req.params;
+        await get();
+        if(id){
+            const info = await product.findAll({
+                where: {
+                    id: {[Op.iLike]: `%${id}%`}
+                }
+            })
+          res.status(201).json(info);
+        }
+    } catch (error){
+        res.status(404).json({error: error.message});
+    }
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createProduct } from "../controllers/createProduct.controller.js";
 import { patchProducts } from "../controllers/patchProducts.controller.js";
+import { idProduct } from "../controllers/idProduct.controller.js";
 
 const router = Router();
 
 router.post("/products", createProduct);
 router.patch("/products/:id", patchProducts);
-
+router.get("products/:id", idProduct);
 
 export default router;
