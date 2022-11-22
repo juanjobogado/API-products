73f45a6728c0e177a3792ecc653f9cd877a8ed17
Jotanjo <maza011000@gmail.com>
2022-11-21 22:42:58 -0300

    modelitos

---
+++ src/models/products.js
@@ -0,0 +1,31 @@
+import  sequelize  from "../database/database"
+import { DataTypes } from "sequelize/types"
+
+export const product = sequelize.define("product",{
+    id:{
+        type: DataTypes.INTEGER,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    title:{
+        type: DataTypes.STRING(67),
+        allowNull:  false,
+        unique: true
+    },
+    prince:{
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    stock:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    categories:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description:{
+        type: DataTypes.STRING,
+        defaultValue: "Aparently we have no description to offer you, please tell us if we don´t"
+    }
+}, {timestamps: false})
