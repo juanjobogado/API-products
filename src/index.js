252a9b13c65c6e05603dcd6eac79c637df768f2d
juanjobogado <104685460+juanjobogado@users.noreply.github.com>
2022-11-23 12:36:40 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,25 +8,25 @@ sequelize.sync({ force: true }).then(() => {
   app.listen(process.env.DB_PORT, () => {
     console.log(`%s listening at, ${process.env.DB_PORT}`);
   });
-  let arrayProducts = product.findAll();
-  if(!arrayProducts.length){
-    fs.readFile("Products.json", (error, data) => {
-      if (error) throw error;
-      let json = JSON.parse(data);
-      let arr = json.results?.map((e) => {
+//   let arrayProducts = product.findAll();
+//   if(!arrayProducts.length){
+//     fs.readFile("Products.json", (error, data) => {
+//       if (error) throw error;
+//       let json = JSON.parse(data);
+//       let arr = json.results?.map((e) => {
   
-        return product.create({
-          id: e.id,
-          title: e.title,
-          price: e.price,
-          image: e.image,
-          stock: e.stock,
-          categories: e.categories,
-          description: e.description,
-        })
-      })
-      Promise.all(arr)
-    })
-  }
+//         return product.create({
+//           id: e.id,
+//           title: e.title,
+//           price: e.price,
+//           image: e.image,
+//           stock: e.stock,
+//           categories: e.categories,
+//           description: e.description,
+//         })
+//       })
+//       Promise.all(arr)
+//     })
+//   }
   
 });
