38e94bc03f15dc58cd98260513895ffdb1b1356a
juanjobogado <juanjobogado13@gmail.com>
2022-11-25 18:40:46 -0300

    los odio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import productRoutes from './routes/products.routes.js'
+import morgan from "morgan";
+
 
 const app = express();
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(productRoutes);
--- src/controllers/idProduct.controller.js
+++ src/controllers/idProduct.controller.js
@@ -9,7 +9,7 @@ export const idProduct = async (req,res) => {
                         id: id
                 }
             })
-          res.status(201).json(info);
+          res.status(200).json(info);
         }
     } catch (error){
         res.status(404).json({error: error.message});
