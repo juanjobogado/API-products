19c558e1558d5bdb354f9bc1f53004dfeae2cc88
NicoxRC <nicorojas005@gmail.com>
2022-11-25 09:06:13 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import productRoutes from './routes/products.routes.js'
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(productRoutes);
 
 export default app;
