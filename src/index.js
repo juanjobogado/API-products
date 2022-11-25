840324ccea261a6c00abaad4a6c0cf393ed2ccaf
juanjobogado <juanjobogado13@gmail.com>
2022-11-25 19:55:46 -0300

    force false

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { product } from './models/products.js'
 import { sequelize } from "./database/database.js";
 
 
-sequelize.sync({ force: true }).then( async () => {
+sequelize.sync({ force: false }).then( async () => {
   app.listen(process.env.PORT, () => {
     console.log(`%s listening at, ${process.env.PORT}`);
   });
