6b615f4c6ac4109897734a06297470e1c0f75c16
juanjobogado <juanjobogado13@gmail.com>
2022-11-29 19:42:31 -0300

    post working with cloudinary

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,11 +4,15 @@ import { getAllProducts } from "../controllers/getAllProducts.controller.js";
 import { patchProducts } from "../controllers/patchProducts.controller.js";
 import { getCategories } from "../controllers/getCategories.controller.js"
 import { idProduct } from "../controllers/idProduct.controller.js";
+import fileUpload from "express-fileupload"
 
 const router = Router();
 
 router.get("/products", getAllProducts);
-router.post("/products", createProduct);
+router.post("/products",fileUpload({
+    useTempFiles:true,
+    tempFileDir:"./uploads"
+  }), createProduct);
 router.patch("/products/:id", patchProducts);
 router.get("/categories", getCategories);
 router.get("/products/:id", idProduct);
