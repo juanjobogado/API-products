7fef29678c2229345392d476e46cb92bd1852616
NicoxRC <nicorojas005@gmail.com>
2022-11-29 11:39:43 -0500

    delete route

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -4,6 +4,7 @@ import { getAllProducts } from "../controllers/getAllProducts.controller.js";
 import { patchProducts } from "../controllers/patchProducts.controller.js";
 import { getCategories } from "../controllers/getCategories.controller.js"
 import { idProduct } from "../controllers/idProduct.controller.js";
+import { deleteProduct } from "../controllers/deleteProduct.controller.js";
 
 const router = Router();
 
@@ -12,4 +13,5 @@ router.post("/products", createProduct);
 router.patch("/products/:id", patchProducts);
 router.get("/categories", getCategories);
 router.get("/products/:id", idProduct);
+router.delete("/products/:id", deleteProduct)
 export default router;
