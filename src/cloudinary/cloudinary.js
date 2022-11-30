15348931f157e68a19def8897653cfc8bb571a60
juanjobogado <juanjobogado13@gmail.com>
2022-11-30 18:17:22 -0300

    cloudinary working

--- src/cloudinary/cloudinary.js
+++ src/cloudinary/cloudinary.js
@@ -1,13 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config()
-import  cloudinary from "cloudinary";
-cloudinary.v2;
+import { v2 as cloudinary } from 'cloudinary'
 const { API_KEY, API_SECRET, CLOUD_NAME } = process.env;
 
 cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET,
+  cloud_name:CLOUD_NAME,
+  api_key:API_KEY,
+  api_secret:API_SECRET,
   secure: true,
 });
 
