cf8eee7c212086bf901271e9d9c8306c0c3ac88c
NicoxRC <nicorojas005@gmail.com>
2022-12-01 10:27:07 -0500

    prueba

--- src/controllers/createProduct.controller.js
+++ src/controllers/createProduct.controller.js
@@ -28,26 +28,26 @@ import fs from "fs";
 export const createProduct = async (req, res) => {
     try {
         // const {image} = req.files
-        const { title, price, stock, categories, description} = req.body;
+        const { title, price, stock, categories, description } = req.body;
         if (!title || !price || !stock || !categories || !description) throw new Error("bad request");
         console.log(req.files);
         console.log(req.body, "aqui");
-         let products = {title, price, stock, categories, description};
-         if (req.files) {
+        let products = { title, price, stock, categories, description };
+        if (req.files) {
             console.log("entra")
             const imageUploaded = await uploadImage(req.files.image.tempFilePath);
             let id = imageUploaded.public_id;
             let url = imageUploaded.url;
             products.url = id;
             products.image = url;
-            await fs.unlink(req.files.image.tempFilePath, err =>{
-                if(err) console.log(err)
-              });
-          }
+            await fs.unlink(req.files.image.tempFilePath, err => {
+                if (err) console.log(err)
+            });
+        }
 
         const newProduct = await product.create(products)
 
-         res.status(201).send("done");
+        res.status(201).send(newProduct);
     } catch (error) {
         console.log(error)
         res.status(400).json({ error: error.message })
