a327f9650accd81d2f87596108e890a7cff89560
userJAMT <jmendez4tob@gmail.com>
2022-12-01 17:09:58 -0300

    get all products updated to work combinated

--- src/controllers/getAllProducts.controller.js
+++ src/controllers/getAllProducts.controller.js
@@ -8,31 +8,23 @@ export const getAllProducts = async (req, res) => {
     const maxPrice = req.query.maxPrice
     const orderAttribute = req.query.orderAttribute
     const orderDirection = req.query.orderDirection
-    const categories = req.query.categories 
-    
-    if(!name){
-      let conditions = {} //Order and Where
-      let where = {} //Filters
+    const categories = req.query.categories
 
-      if (minPrice && maxPrice) where.price = {[Op.between]:[minPrice, maxPrice]}
-      if (categories) where.categories = categories
-      if (orderAttribute && orderDirection) conditions.order = [[orderAttribute, orderDirection]]
-      if (where !== {}) conditions.where = where
+    let conditions = {} //Order and Where
+    let where = {} //Filters
 
-      let data = await product.findAll(conditions);
+    if (minPrice && maxPrice) where.price = { [Op.between]: [minPrice, maxPrice] }
+    if (categories) where.categories = categories
+    if (name) where.title = { [Op.iLike]: `%${name}%` }
+    if (orderAttribute && orderDirection) conditions.order = [[orderAttribute, orderDirection]]
+    if (where !== {}) conditions.where = where
 
-      if (!data.length) return res.status(404).json({msg: "Products not found"})
-  
-      res.status(200).json(data);
-    } else {
-      const info = await product.findAll({
-        where: {
-          title: {[Op.iLike]: `%${name}%`}
-        }
-    })
-  res.status(201).json(info);
-    }
-    
+    let data = await product.findAll(conditions);
+
+    if (!data.length) return res.status(404).json({ msg: "Products not found" })
+
+    res.status(200).json(data);
+
   } catch (error) {
     res.status(500).send({ msg: "Server error", error: error.message });
   }
