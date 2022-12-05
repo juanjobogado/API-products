21d20f23609df2601b02052abffbebe2095047f4
userJAMT <jmendez4tob@gmail.com>
2022-12-04 21:18:18 -0300

    extended limit for json and urlencoded request

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from "express";
-import productRoutes from './routes/products.routes.js'
+import productRoutes from "./routes/products.routes.js";
 import morgan from "morgan";
 import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+);
 app.use(cors());
 app.use(productRoutes);
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 export default app;
